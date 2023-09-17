caa76f929e6c637da19d71152c8a79714b9f944f
AkhilReddy-0315 <136949385+AkhilReddy-0315@users.noreply.github.com>
2023-09-17 10:02:40 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ const App = () => (
   <div className="app-con">
     <Switch>
       <Route exact path="/" component={Home} />
-      <Route exact path="/ipl/:id" component={TeamMatches} />
+      <Route exact path="/team-matches/:id" component={TeamMatches} />
       <Route component={NotFound} />
     </Switch>
   </div>
